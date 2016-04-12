596857c902009d25184657b2f640cfcdc598f05c
Carl Blanchard <carl.blanchard@katiya.com>
2016-04-12 21:02:32 +0100

    Initial Commit - Just testing this code is a demo MVC project generated by visual studio

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Git_Hub_Test
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
